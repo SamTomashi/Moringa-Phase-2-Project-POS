66c7f8ea3feff61382809dccc3fa9927a89361cd
SamTomashi <samtomashikayembi@yahoo.fr>
2022-11-10 17:25:53 +0300

    add labels

--- src/components/OrderLine.js
+++ src/components/OrderLine.js
@@ -3,21 +3,25 @@ import React from "react"
 function OrderLine(){
 
     const row = <React.Fragment>
-        <div className="row">
+        <div className="mb-3 row">
             <div className="col">
-                <select name="item" onChange={()=>{}}>
+            <small className=" text-muted">Item</small><br></br>
+                <select className="form-select" name="item" onChange={()=>{}}>
                     <option value="item 1">Item 1</option>
                     <option value="item 2">Item 2</option>
                 </select>
             </div>
             <div className="col">
-                <input name="price" onChange={()=>{}} type="text" readOnly/>
+            <small className=" text-muted">Price</small><br></br>
+                <input className="form-control form-control-sm" name="price" onChange={()=>{}} type="text" readOnly/>
             </div>
             <div className="col">
-                <input name="qty" onChange={()=>{}} type="number" value="1" min="1"/>
+                <small className=" text-muted">QTY</small><br></br>
+                <input className="form-control form-control-sm" name="qty" onChange={()=>{}} type="number" value="1" min="1"/>
             </div>
             <div className="col">
-                <input name="lineTotal" readOnly onChange={()=>{}} type="text" value=""/>
+                <small className=" text-muted">Total</small><br></br>
+                <input className="form-control form-control-sm" name="lineTotal" readOnly onChange={()=>{}} type="text" value=""/>
             </div>
         </div>
     </React.Fragment>
