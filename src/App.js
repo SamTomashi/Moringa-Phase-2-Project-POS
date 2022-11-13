bb65b312beec3bb2e51ddee5a63f9ba0071759ea
SamTomashi <samtomashikayembi@yahoo.fr>
2022-11-13 13:41:00 +0300

    import icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import Header from './components/Header'
 import Orders from './components/Orders';
 import Footer from './components/Footer';
-library.add(fab, faPlus)
+library.add(fab, faPlus, faMinus)
 
 function App() {
   return (
