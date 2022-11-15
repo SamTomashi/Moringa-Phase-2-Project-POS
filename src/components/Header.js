fcd521da7014c590ed3d0053ad6a52b189d9ad05
SamTomashi <samtomashikayembi@yahoo.fr>
2022-11-15 21:48:18 +0300

    Add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,25 @@
+import {Link} from "react-router-dom";
 function Header(){
     return(
-        <div></div>
+        <nav className="navbar navbar-dark bg-dark mb-3" aria-label="First navbar example">
+    <div className="container-fluid">
+      <a className="navbar-brand text-uppercase" href="/">Supermarket</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarsExample01">
+        <ul className="navbar-nav me-auto mb-2">
+            <li className="nav-item">
+                <Link className="nav-link" to="/">POS</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/orders">Orders</Link>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
     )
 }
 
