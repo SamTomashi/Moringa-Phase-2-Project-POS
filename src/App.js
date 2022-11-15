ef738ba3dfb51906acff214b7fd023d31e577fc6
SamTomashi <samtomashikayembi@yahoo.fr>
2022-11-15 21:47:44 +0300

    implement routing

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
 import Header from './components/Header'
-import Orders from './components/Orders';
+import Pos from './components/Pos';
 import Footer from './components/Footer';
+import Orders from "./components/Orders";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 library.add(fab, faPlus, faMinus)
 
 function App() {
   return (
     
     <div className="container">
-        <Header/>
-        <Orders/>
+        <Router>
+          <Header/>
+          <Routes>
+            <Route exact path='/' element={<Pos/>} />
+            <Route exact path='/orders' element={<Orders/>} />
+          </Routes>
+        </Router>
         <Footer/>
     </div>
+    
   );
 }
 
